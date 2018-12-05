2233aca18856559c7ae31b96c37d5236ff111815
Nina Maller <nina.maller@gmail.com>
2018-12-04 16:18:08 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.	  
+          To get started, edit <code>src/App.js</code> and save to reload.
+          Editing the conent of the file and comitting it.
         </p>
       </div>
     );
